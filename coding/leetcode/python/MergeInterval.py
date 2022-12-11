429bb30463beab0d92bb70548102d5c79c3b4a38
rohitpjpti18 <rohitpjpti18@gmail.com>
2022-12-11 09:47:32 +0530

    improved merge intervals in python

--- coding/leetcode/python/MergeInterval.py
+++ coding/leetcode/python/MergeInterval.py
@@ -2,27 +2,17 @@ from functools import cmp_to_key
 from typing import List
 
 class Solution:
-
-    def comparator(self, a:List[int], b:List[int]) -> int:
-        if a[0] != b[0]:
-            return a[0]-b[0]
-        else:
-            return a[1]-b[1]
-
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=cmp_to_key(self.comparator));
+        intervals = sorted(intervals, key=lambda x: x[0]);
         print(intervals)
         
         result = []
         result.append(intervals[0])
 
         for i in range(1, len(intervals)):
-            temp = result[-1]
-            if intervals[i][0] <= temp[1] and intervals[i][1] > temp[1]:
-                temp[1] = intervals[i][1]
-                continue;
-            
-            if intervals[i][0] > temp[1]:
-                result.append(intervals[i])
+            if(result[-1][1] < intervals[i][0]):
+                result.append(intervals[i]);
+            else:
+                result[-1][1] = max(result[-1][1], intervals[i][1]);
         
         return result
